e5191f7df7eea2df9a2bee6b272883e94955ce39
parimal <p.mahindrakar@kokonetworks.com>
2024-12-31 11:01:33 +0530

    [FEATURE] Build optimized Money Tracker API - Write the logic to delete the bank object

--- src/blueprints/banks.py
+++ src/blueprints/banks.py
@@ -4,12 +4,14 @@ from flask_cors import CORS
 from json import loads
 from datetime import datetime
 from mongoengine import ValidationError
+from bson import ObjectId
 
 # relative imports
 from src.models.bank import Bank
 from src.constants import DATE_TIME_FORMAT
 from src.schemas.schemas import BankSchema
 from src.helpers.authentication import token_required
+from src.models.expense import Expense
 
 bank_bp = Blueprint("banks", __name__)
 CORS(bank_bp, resources={r"/*": {"origins": "*"}})
@@ -38,3 +40,22 @@ def create_bank(current_user):
         return jsonify({"error": [obj.message for _, obj in err.errors.items()]}), 400
     except Exception as err:
         return jsonify({"error": "Error while saving the bank object"})
+
+
+@bank_bp.delete("/delete")
+@token_required
+def delete_bank(current_user):
+    data = loads(request.data.decode("utf-8"))
+    if bank_id := data.get("bank_id"):
+        if bank := Bank.objects(id=bank_id, user_id=current_user.id).first():
+            if expenses := Expense.objects(
+                bank=ObjectId(bank_id), user_id=current_user.id
+            ):
+                if expenses.count() > 0:
+                    expenses.delete()
+            bank.delete()
+            return jsonify({"success": "Bank deleted successfully"})
+        else:
+            return jsonify({"error": "Bank not found with provided ID"}), 400
+    else:
+        return jsonify({"error": "Please provide the bank id to delete"}), 400
