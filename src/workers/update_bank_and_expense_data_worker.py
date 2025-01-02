54dbe5c35e8dc31939060856085a5d2d7547219f
parimal <p.mahindrakar@kokonetworks.com>
2025-01-02 20:34:08 +0530

    [FEATURE] Build optimized Money Tracker API - Fix the bug for which upper expenses were not getting updated if added in middle

--- src/workers/update_bank_and_expense_data_worker.py
+++ src/workers/update_bank_and_expense_data_worker.py
@@ -23,42 +23,58 @@ def update_bank_and_expense_data(
     current_user = User.objects(id=ObjectId(user_id)).first()
     helper.configure_logging(current_app)
     current_app.logger.info(
-        f"\nCurrent user id : {str(current_user.id)}"\
-        f"\nFile name: {__name__}"
+        f"\nCurrent user id : {str(current_user.id)}" f"\nFile name: {__name__}"
     )
     if current_user and bank_id and expense_id:
         bank = Bank.objects(id=bank_id).first()
         expense = Expense.objects(id=expense_id).first()
+        previous_expense = (
+            Expense.objects(
+                created_at__lt=expense.created_at, bank=bank.id, user_id=current_user.id
+            )
+            .order_by("-created_at")
+            .first()
+        )
+        above_expenses = Expense.objects(
+            created_at__gt=expense.created_at, bank=bank.id, user_id=current_user.id
+        ).order_by("-created_at")
 
         if bank and expense:
             expense_data_to_update = {}
             bank_data_to_update = {}
+            if previous_expense:
+                amount_from_which_deducted_from = (
+                    previous_expense.remaining_amount_till_now
+                )
+            else:
+                amount_from_which_deducted_from = bank.current_balance
 
             if is_newly_created:
-                remaining_amount_till_now = bank.current_balance - expense.expense_total
                 total_disbursed_till_now = (
                     bank.total_disbursed_till_now + expense.expense_total
                 )
 
                 expense_data_to_update.update(
-                    set__remaining_amount_till_now=remaining_amount_till_now,
+                    set__remaining_amount_till_now=amount_from_which_deducted_from
+                    - expense.expense_total,
                     set__updated_at=datetime(
                         *helper.provide_todays_date(str_format=False)
                     ),
                 )
                 bank_data_to_update.update(
                     set__total_disbursed_till_now=total_disbursed_till_now,
-                    set__current_balance=remaining_amount_till_now,
+                    set__current_balance=bank.current_balance - expense.expense_total,
                     set__updated_at=datetime(
                         *helper.provide_todays_date(str_format=False)
                     ),
                 )
+                if above_expenses:
+                    above_expenses.update(
+                        **{"inc__remaining_amount_till_now": -(expense.expense_total)},
+                        multi=True,
+                    )
             else:
                 if total_entry_entered is not None:
-                    expense_bank_current_balance = (
-                        bank.current_balance - total_entry_entered
-                    )
-
                     expense_data_to_update.update(
                         set__expense_total=(
                             expense.expense_total + total_entry_entered
@@ -71,9 +87,8 @@ def update_bank_and_expense_data(
                         set__total_disbursed_till_now=(
                             bank.total_disbursed_till_now + total_entry_entered
                         ),
-                        set__current_balance=expense_bank_current_balance,
+                        set__current_balance=bank.current_balance - total_entry_entered,
                     )
-
                     upper_expenses = Expense.get_expenses(
                         **dict(
                             start_date=helper.provide_todays_date(
