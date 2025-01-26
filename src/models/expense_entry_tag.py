243c89827629d99b7c3444cef14ed1e7d12e1574
parimal <p.mahindrakar@kokonetworks.com>
2025-01-26 14:30:30 +0530

    [FEATURE] Build optimized Money Tracker API - Write the test cases for ExpenseEntryTags

--- src/models/expense_entry_tag.py
+++ src/models/expense_entry_tag.py
@@ -4,3 +4,20 @@ from mongoengine import Document, StringField, ObjectIdField
 class ExpenseEntryTag(Document):
     name = StringField()
     user_id = ObjectIdField()
+
+    @classmethod
+    def get_tags(cls, current_user, **data):
+        query = {"user_id": current_user.id}
+        if data.get("name"):
+            query["name"] = (
+                {"$regex": data["name"]} if data.get("regex") else data["name"]
+            )
+        elif data.get("id"):
+            query["id"] = data["id"]
+
+        return list(
+            map(
+                lambda tag_object: {"id": str(tag_object.id), "name": tag_object.name},
+                ExpenseEntryTag.objects(**query),
+            )
+        )
--- tests/functional/helper/helper.py
+++ tests/functional/helper/helper.py
@@ -1,104 +1,116 @@
-class ExpenseHelper:
+class BaseHelper:
+    def __init__(self):
+        self.headers = {
+            "Content-Type": "application/json",
+        }
+
+
+class ExpenseHelper(BaseHelper):
     def __init__(self, app):
+        super().__init__()
         self.testapp = app
 
     def get_expenses_api_call(self, api_token, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.get(
-            "/expenses/", headers=headers, params=params, expect_errors=True
+            "/expenses/", headers=self.headers, params=params, expect_errors=True
         )
 
     def create_expense_api(self, api_token, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.post(
-            "/expenses/create", headers=headers, params=params, expect_errors=True
+            "/expenses/create", headers=self.headers, params=params, expect_errors=True
         )
 
     def create_expense_entry_api(self, api_token, expense_id, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.patch(
             f"/expenses/add-entry?id={expense_id}",
-            headers=headers,
+            headers=self.headers,
             params=params,
             expect_errors=True,
         )
 
     def delete_expense_api(self, api_token, expense_id, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.delete(
             f"/expenses/delete?id={expense_id}",
-            headers=headers,
+            headers=self.headers,
             params=params,
             expect_errors=True,
         )
 
     def delete_expense_entry_api(self, api_token, expense_id, ee_id, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.delete(
             f"/expenses/delete-entry?id={expense_id}&ee_id={ee_id}",
-            headers=headers,
+            headers=self.headers,
             params=params,
             expect_errors=True,
         )
 
     def update_expense_entry_api(self, api_token, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.patch(
             f"/expenses/update-entry",
-            headers=headers,
+            headers=self.headers,
             params=params,
             expect_errors=True,
         )
 
 
-class BanksHelper:
+class BanksHelper(BaseHelper):
     def __init__(self, app):
+        super().__init__()
         self.testapp = app
 
     def get_banks_api_call(self, api_token, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.get(
-            "/banks/", headers=headers, params=params, expect_errors=True
+            "/banks/", headers=self.headers, params=params, expect_errors=True
         )
 
     def post_banks_api_call(self, api_token, params={}):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.post(
-            "/banks/create", headers=headers, params=params, expect_errors=True
+            "/banks/create", headers=self.headers, params=params, expect_errors=True
         )
 
     def delete_banks_api_call(self, api_token, bank_id):
-        headers = {
-            "x-access-token": api_token,
-            "Content-Type": "application/json",
-        }
+        self.headers["x-access-token"] = api_token
         return self.testapp.delete(
             f"/banks/delete?bank_id={bank_id}",
-            headers=headers,
+            headers=self.headers,
             params={},
+            expect_errors=True,
+        )
+
+
+class ExpenseEntryTagsHelper(BaseHelper):
+    def __init__(self, app):
+        super().__init__()
+        self.testapp = app
+
+    def get_entry_tags_api_call(self, api_token, params={}):
+        self.headers["x-access-token"] = api_token
+        return self.testapp.get(
+            "/entry-tags/", headers=self.headers, params=params, expect_errors=True
+        )
+
+    def create_entry_tags_api_call(self, api_token, params={}):
+        self.headers["x-access-token"] = api_token
+        return self.testapp.post(
+            "/entry-tags/create",
+            headers=self.headers,
+            params=params,
+            expect_errors=True,
+        )
+
+    def delete_entry_tags_api_call(self, api_token, params={}):
+        self.headers["x-access-token"] = api_token
+        return self.testapp.delete(
+            "/entry-tags/delete",
+            headers=self.headers,
+            params=params,
             expect_errors=True,
         )
