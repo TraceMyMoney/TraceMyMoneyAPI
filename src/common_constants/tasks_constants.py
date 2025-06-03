a0e1eae3deb8d6c1e1c453ad7c563e133ea55689
parimal <p.mahindrakar@kokonetworks.com>
2025-06-03 08:59:58 +0530

    [FEATURE] Aggregate data and send data via email - Reset the task constants

--- src/common_constants/tasks_constants.py
+++ src/common_constants/tasks_constants.py
@@ -3,14 +3,14 @@ from celery.schedules import crontab
 SCHEDULED_TASKS = {
     "send_daily_expenses_data": {
         "task": "src.scheduled_tasks.tasks.send_daily_expenses_data",
-        "schedule": crontab(minute="*/1"),
+        "schedule": crontab(hour=0, minute=1),
     },
     "send_weekly_expenses_data": {
         "task": "src.scheduled_tasks.tasks.send_weekly_expenses_data",
-        "schedule": crontab(minute="*/2"),
+        "schedule": crontab(hour=0, minute=1, day_of_week=0),
     },
     "send_monthly_expenses_data": {
         "task": "src.scheduled_tasks.tasks.send_monthly_expenses_data",
-        "schedule": crontab(minute="*/3"),
+        "schedule": crontab(hour=0, minute=1, day_of_month=1),
     },
 }
