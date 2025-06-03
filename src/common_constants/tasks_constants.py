df9aacbcd6b3c149dcb22fce49ca23971af41d1d
parimal <p.mahindrakar@kokonetworks.com>
2025-06-03 08:54:19 +0530

    [FEATURE] Aggregate data and send data via email - Fix docker compose for flask-app start on condition

--- src/common_constants/tasks_constants.py
+++ src/common_constants/tasks_constants.py
@@ -3,14 +3,14 @@ from celery.schedules import crontab
 SCHEDULED_TASKS = {
     "send_daily_expenses_data": {
         "task": "src.scheduled_tasks.tasks.send_daily_expenses_data",
-        "schedule": crontab(hour=0, minute=1),
+        "schedule": crontab(minute="*/1"),
     },
     "send_weekly_expenses_data": {
         "task": "src.scheduled_tasks.tasks.send_weekly_expenses_data",
-        "schedule": crontab(hour=0, minute=1, day_of_week=0),
+        "schedule": crontab(minute="*/2"),
     },
     "send_monthly_expenses_data": {
         "task": "src.scheduled_tasks.tasks.send_monthly_expenses_data",
-        "schedule": crontab(hour=0, minute=1, day_of_month=1),
+        "schedule": crontab(minute="*/3"),
     },
 }
